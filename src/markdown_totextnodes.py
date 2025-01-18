56c6d024927957da40ddb873e473f22b8ee6630f
Aklantan <allan.tucker@gmail.com>
2025-01-18 11:50:29 +0000

    following boots advice

--- src/markdown_totextnodes.py
+++ src/markdown_totextnodes.py
@@ -1,6 +1,6 @@
 from htmlnode import *
 from textnode import *
-
+import re
 
 def split_nodes_delimiter(old_nodes, delimiter,text_type):
     new_nodes = []     
@@ -23,6 +23,57 @@ def split_nodes_delimiter(old_nodes, delimiter,text_type):
     return new_nodes
 
 
+def split_nodes_image(old_nodes):
+    new_nodes = []     
+        
+    for node in old_nodes:
+        image_tags = extract_markdown_images(node.text)
+        for i in range(len(image_tags)):
+            return new_nodes
+
+def extract_markdown_images(text):
+    validate_no_nested_brackets(text)
+    validate_no_nested_parentheses(text)
+    images = re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+    return images
+
+    
+def extract_markdown_links(text):
+    validate_no_nested_brackets(text)
+    validate_no_nested_parentheses(text)
+    links = re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+    return links
+
+def validate_no_nested_brackets(text):
+    bracket_depth = 0
+    for char in text:
+        if char == "[":
+            bracket_depth += 1
+            if bracket_depth > 1:  # We've nested brackets
+                raise ValueError("Unable to handle nested brackets.")
+        elif char == "]":
+            bracket_depth -= 1
+    if bracket_depth != 0:
+        raise ValueError("Unmatched brackets detected.")  # For dangling brackets
+
+
+def validate_no_nested_parentheses(text):
+    paren_depth = 0
+    for char in text:
+        if char == "(":
+            paren_depth += 1
+            if paren_depth > 1:  # We've nested parentheses
+                raise ValueError("Unable to handle nested parentheses in URL.")
+        elif char == ")":
+            paren_depth -= 1
+    if paren_depth != 0:
+        raise ValueError("Unmatched parentheses detected.")  # For dangling parentheses
+
+text_with_links = "Here is [a link](https://example.com) and here is [an image](https://example.com/image.png)."
+print(extract_markdown_links(text_with_links))
+
+text_with_images = "Check this out ![Alt text](https://example.com/image.png) and ![Another](https://example.com/another.png)."
+print(extract_markdown_images(text_with_images))
 
 
    
